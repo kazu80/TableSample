83dd2e11f3c01ff0281ec39e458fe58fee377ff0
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-27 09:27:42 +0900

    make project
    プロジェクト作成

---
+++ TableSample/TableSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableSample
+//
+//  Created by kazuyoshi kawakami on 13/06/27.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
