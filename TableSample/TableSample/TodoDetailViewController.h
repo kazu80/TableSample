f4fbaf8cdd67833de5ca9d1d9aeed550773313ad
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-07-08 11:31:53 +0900

    Add edit extention
    編集機能の追加

---
+++ TableSample/TableSample/TodoDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  TodoDetailViewController.h
+//  TableSample
+//
+//  Created by kazuyoshi kawakami on 13/07/08.
+//  Copyright (c) 2013年 kazuyoshi kawakami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Todo.h"
+
+@interface TodoDetailViewController : UIViewController
+@property (strong, nonatomic) Todo * todo;
+
+@end
